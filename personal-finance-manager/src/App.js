28d0313af77841f0eebb325ca9143908bedca799
CynanRanjiwa <NancyWanjira2894@gmail.com>
2024-09-24 15:44:46 +0300

    importing the components and adding basic layout

--- personal-finance-manager/src/App.js
+++ personal-finance-manager/src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import ExpenseForm from './components/ExpenseForm';
+import IncomeForm from './components/ExpenseForm';
+import BudgetOverview from './components/BudgetOverview';
+
+// main app component to render all other components 
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div className="form-container">
+        <IncomeForm />
+        <ExpenseForm />
     </div>
-  );
+    <BudgetOverview />
+    </div>
+  )
 }
 
 export default App;
---
+++ personal-finance-manager/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// header componet displays the app title 
+const Header = () => {
+    return (
+        <header>
+            <h1>Personal Finance Manager</h1>
+        </header>
+    );
+};
+
+export default Header;
