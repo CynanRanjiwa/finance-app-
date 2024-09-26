848ad1ea938aa4ec170076de44684e1b391ba0a6
CynanRanjiwa <NancyWanjira2894@gmail.com>
2024-09-26 08:29:48 +0300

    handling income and expense state

--- personal-finance-manager/src/App.js
+++ personal-finance-manager/src/App.js
@@ -24,23 +24,39 @@
 
 // export default App;
 
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 import ExpenseForm from './components/ExpenseForm';
 import IncomeForm from './components/ExpenseForm';
 import BudgetOverview from './components/BudgetOverview';
+import FinanceChart from './components/FinanceChart';
 
 // main app component to render all other components 
 const App = () => {
+  //state to track list of incomes ad expenses 
+  const [incomes, setIncomes] = useState([]);
+  const [expenses, setExpenses] = useState([]);
+
+  // add new income 
+  const addIncome = (source, amount) => {
+    setIncomes([...incomes, {source, amount}]);
+  };
+  // add new expenses 
+  const addExpense = (category, amount) => {
+    setExpenses([...expenses, {category, amount}]);
+  };
+
+  // render the app with all components 
   return (
     <div className="App">
       <Header />
       <div className="form-container">
-        <IncomeForm />
-        <ExpenseForm />
-    </div>
-    <BudgetOverview />
+        <IncomeForm addIncome={addIncome} />
+        <ExpenseForm addExpense={addExpense} />
+      </div>
+      <BudgetOverview incomes={incomes} expenses={expenses} />
+      <FinanceChart incomes={incomes} expenses={expenses} />
     </div>
   )
 }
